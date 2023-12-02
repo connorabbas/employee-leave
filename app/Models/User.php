8acfdd2b9de6e4ed4c19d932a37039e41278b520
Connor Abbas <abbasconnor@gmail.com>
2023-12-02 12:38:28 -0600

    misc

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'department_id',
+        'location_id',
+        'is_admin',
     ];
     protected $hidden = [
         'password',
